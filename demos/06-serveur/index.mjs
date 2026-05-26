4e1f832f8bc0966b7d422d4b3dee44c55e07f96c
formateur-glodie-tshimini <contact@tshimini.fr>
2026-05-26 16:11:26 +0200

    refactor: add comments demo web server

--- demos/06-serveur/index.mjs
+++ demos/06-serveur/index.mjs
@@ -1,14 +1,17 @@
 /**
  * 1. Importer la fonction createServer() depuis node:http
- * 2. Appeler la fonction avec les méthodes suivantes:
- *  - listen() : spécifie le port d'écoute => Les clients peuvent se connecter depuis un hôte et un PORT
+ * 2. Appeler la fonction d'abord cette fonction createServer puis la chaîner avec listen()
+ *  - listen() : spécifie l'hôte et le port d'écoute => Les clients peuvent se connecter depuis cette adresse http://hote:port
+ *    - ici hote = localhost (valeur par défaut)
+ *    - Le port on va utiliser un PORT disponible sur notre machine par ex 8088 
  * 
  */
-import { createServer } from 'node:http'
+import { createServer } from 'node:http';
 
 const PORT = 8088 // Port disponible sur la machine hôte
-createServer((req, res) => {// une callback de createServer => au moment
+createServer((req, res) => {
+  // Une callback de createServer => au moment d'une connexion d'un client, cette callback est exécutée
   res.end('<h1>Bonjour</h1>') // réponse retournée au client
-}).listen(PORT, () => { // callback
+}).listen(PORT, () => { // callback exécuté au lancement de l'application
   console.info(`Running at http://localhost:${PORT}`)
 })
