e94ffc202e9609b9a07b4874c2ee6ef396522c8c
glodie <glodie.tshimini@gmail.com>
2026-05-28 11:52:37 +0200

    fix: router refactoring

--- exercices/corrections/5.1-refactoring/routes/users-github.js
+++ exercices/corrections/5.1-refactoring/routes/users-github.js
@@ -1,25 +1,9 @@
 const express = require('express')
 const router = express.Router()
-
+const { getUsers, getOneUser } = require('../controllers/users-github-controller.cjs')
 // match avec la route /github
-router.get('/', (req, res) => {
-    fetch('https://api.github.com/users')
-    .then(res => res.json())
-    .then(users => {
-        console.log('users', users)
-        res.render('users/list', { users })
-    })
-    .catch(() => res.render('error', { message: 'Aucun utilisateur', status: 500 }))
- // callback qui s'exécute sur cette route quand un utilisateur fera une req GET /users-github
-})
-
+router.get('/', getUsers)
 // match avec GET /github/:login avec login dynamique
-router.get('/:login', async (req, res) => {
-    const login = req.params.login
-    const user = await fetch(`https://api.github.com/users/${login}`).then(data => data.json())
-    console.log('user', user)
-    res.render('users/single', { user })
-})
-
+router.get('/:login', getOneUser)
 
 module.exports = router
--- exercices/corrections/5.1/routes/users-github.js
+++ exercices/corrections/5.1/routes/users-github.js
@@ -1,9 +1,35 @@
 const express = require('express')
 const router = express.Router()
-const { getUsers, getOneUser } = require('../controllers/users-github-controller.cjs')
+
+function checkLoginMiddleware(req, res, next) {
+    const login = req.params.login
+    if(login.length > 2) next()
+    else throw new Error('Taille insuffisante pour le login')
+}
+
+function anotherMiddleware(req, res, next) {
+    next()
+}
+
 // match avec la route /github
-router.get('/', getUsers)
+router.get('/', anotherMiddleware, (req, res) => {
+    fetch('https://api.github.com/users')
+    .then(res => res.json())
+    .then(users => {
+        console.log('users', users)
+        res.render('users/list', { users })
+    })
+    .catch(() => res.render('error', { message: 'Aucun utilisateur', status: 500 }))
+ // callback qui s'exécute sur cette route quand un utilisateur fera une req GET /users-github
+})
+
 // match avec GET /github/:login avec login dynamique
-router.get('/:login', getOneUser)
+router.get('/:login', checkLoginMiddleware, anotherMiddleware, async (req, res) => {
+    const login = req.params.login
+    const user = await fetch(`https://api.github.com/users/${login}`).then(data => data.json())
+    console.log('user', user)
+    res.render('users/single', { user })
+})
+
 
 module.exports = router
